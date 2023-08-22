022732ec9b61b32173cf8daef2d043311c40a63d
williamscodeuz <uz.williamscode@gmail.com>
2023-08-23 00:08:44 +0500

    initial commit

---
+++ src/main/java/com/williamscode/blogrestapi/controller/PostController.java
@@ -0,0 +1,16 @@
+package com.williamscode.blogrestapi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+
+    @GetMapping
+    public String test(){
+        return "test";
+    }
+}
---
+++ src/test/java/com/williamscode/blogrestapi/BlogRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williamscode.blogrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
